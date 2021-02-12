ec8e98bd2877c23740d7c1f0b0640c12533a93c4
Ozgur Murat <zgrmrts@gmail.com>
2021-02-12 07:55:50 +0300

    som_mx8qm-eng -> som_mx8qx-eng

--- build_android_10.sh
+++ build_android_10.sh
@@ -1,6 +1,6 @@
 cd ~/var_imx-android-10.0.0_2.5.0/android_build &&
 source build/envsetup.sh &&
-lunch som_mx8qm-eng &&
+lunch som_mx8qx-eng &&
 ./imx-make.sh -j4 2>&1 | tee build1-1.log &&
 echo done
 
