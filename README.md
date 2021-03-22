c4aa5ca2162e54dfb45cc318f37b5c40fabad547
Ozgur Murat <zgrmrts@gmail.com>
2021-03-22 11:27:44 +0300

    flash successful

--- README.md
+++ README.md
@@ -13,3 +13,13 @@
     ; veya
     /build-android_10.sh
 
+    ; kurtarma sd kartindan /usr/bin/install_android calistirip android kurulur ve acilir.
+    ; Settings => About Tablet => Build number
+    ; Keep on tapping until you see a prompt that says "You are now a developer!"
+    ; Settings => System => Advanced => Deveoper options => OEM unlocking
+    adb reboot bootloader
+    sudo fastboot oem unlock
+    ; tekrar reboot sonrasi
+    adb reboot bootloader
+    ./flash_android_10.sh
+
--- flash_android_10.sh
+++ flash_android_10.sh
@@ -1,8 +1,9 @@
-sudo `which fastboot` flash dtbo_a ../var_imx-android-10.0.0_2.5.0/android_build/out/target/product/som_mx8q/dtbo-imx8qm-var-som-lvds.img
-sudo `which fastboot` flash dtbo_b ../var_imx-android-10.0.0_2.5.0/android_build/out/target/product/som_mx8q/dtbo-imx8qm-var-som-lvds.img
-sudo `which fastboot` flash boot_a ../var_imx-android-10.0.0_2.5.0/android_build/out/target/product/som_mx8q/boot.img
-sudo `which fastboot` flash boot_b ../var_imx-android-10.0.0_2.5.0/android_build/out/target/product/som_mx8q/boot.img
-sudo `which fastboot` flash super ../var_imx-android-10.0.0_2.5.0/android_build/out/target/product/som_mx8q/super.img
-sudo `which fastboot` flash vbmeta_a ../var_imx-android-10.0.0_2.5.0/android_build/out/target/product/som_mx8q/vbmeta-imx8qm-var-som-lvds.img --disable-verity
-sudo `which fastboot` flash vbmeta_b ../var_imx-android-10.0.0_2.5.0/android_build/out/target/product/som_mx8q/vbmeta-imx8qm-var-som-lvds.img --disable-verity
-sudo `which fastboot` reboot
+sudo `which fastboot` flash dtbo_a ../var_imx-android-10.0.0_2.5.0/android_build/out/target/product/som_mx8q/dtbo-imx8qm-var-som-lvds.img &&
+sudo `which fastboot` flash dtbo_b ../var_imx-android-10.0.0_2.5.0/android_build/out/target/product/som_mx8q/dtbo-imx8qm-var-som-lvds.img &&
+sudo `which fastboot` flash boot_a ../var_imx-android-10.0.0_2.5.0/android_build/out/target/product/som_mx8q/boot.img &&
+sudo `which fastboot` flash boot_b ../var_imx-android-10.0.0_2.5.0/android_build/out/target/product/som_mx8q/boot.img &&
+sudo `which fastboot` flash super ../var_imx-android-10.0.0_2.5.0/android_build/out/target/product/som_mx8q/super.img &&
+sudo `which fastboot` flash vbmeta_a ../var_imx-android-10.0.0_2.5.0/android_build/out/target/product/som_mx8q/vbmeta-imx8qm-var-som-lvds.img --disable-verity &&
+sudo `which fastboot` flash vbmeta_b ../var_imx-android-10.0.0_2.5.0/android_build/out/target/product/som_mx8q/vbmeta-imx8qm-var-som-lvds.img --disable-verity &&
+sudo `which fastboot` reboot &&
+echo done
