8255e02bdbe39d7cab3a87d4f3ec78edb293fcbc
Ozgur Murat <zgrmrts@gmail.com>
2021-03-22 08:17:51 +0300

    updates

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
-cp ~/.gitconfig gitconfig
-docker build --build-arg userid=$(id -u) --build-arg groupid=$(id -g) --build-arg username=$(id -un) -t android-build .
+cp ~/.gitconfig gitconfig &&
+docker build --build-arg userid=$(id -u) --build-arg groupid=$(id -g) --build-arg username=$(id -un) -t android-build . &&
+rm gitconfig &&
+echo done
 
 
--- download_android_10.sh
+++ download_android_10.sh
@@ -7,7 +7,7 @@ tar xvf ~/Downloads/imx-android-10.0.0_2.5.0.tar.gz &&
 source imx-android-10.0.0_2.5.0/imx_android_setup.sh &&
 mkdir -p "${ROOT_ANDROID_DIR}/android_build/vendor/variscite/" &&
 cd "${ROOT_ANDROID_DIR}/android_build/vendor/variscite/" &&
-git clone https://github.com/varigit/uboot-imx.git -b imx_v2020.04_5.4.24_2.1.0_var02 &&
+git clone https://github.com/varigit/uboot-imx.git -b imx_v2020.04_5.4.24_2.1.0_var01 &&
 git clone https://github.com/varigit/linux-imx kernel_imx -b android-10.0.0_2.5.0-var01 &&
 cd "${ROOT_ANDROID_DIR}/android_build/device" &&
 git clone https://github.com/varigit/MX6x-android.git -b android-10.0.0_2.5.0-var01 variscite &&
